5174da718a7d3abd19d358db90ca3b19f40dd785
Timo Brønseth <timo.bronseth@gmail.com>
2020-01-16 23:33:34 +0100

    testing

---
+++ test2.py
@@ -0,0 +1,16 @@
+# my_list = [1, 2, 3, 4, 5]
+# my_list = iter(my_list)
+#
+# while i := next(my_list):
+#     print(i)
+
+my_list = range(100)
+
+
+def my_generator():
+    n =
+    yield n :=
+
+my_set = [x*2 for x in iter(my_list)]
+
+print(my_set)
